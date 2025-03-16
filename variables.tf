bee671086ae1ee397122a4f27dcf818efb0f5fa6
preeti <preetijobs7@gmail.com>
2025-03-16 20:35:31 +0000

    third commit

---
+++ variables.tf
@@ -0,0 +1,26 @@
+/*variable "bucket_name" {
+    description = "This is the bucket name for S3"
+    default = "my-static-website-bucket-15032025"
+}
+
+variable "website_index_document" {
+  default = "index.html"
+}*/
+
+variable "aws_access_key_id" {
+  type = string
+  description = "AWS Access Key ID"
+  sensitive   = true
+}
+
+variable "aws_secret_access_key" {
+  type        = string
+  description = "AWS Secret Access Key"
+  sensitive   = true
+}
+
+variable "aws_instance_names" {
+  type = list(string)
+  description = "instance names"
+  default = ["webserve01","webserve02","webserve03"]
+}
